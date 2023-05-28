12a66cb5cd256a42a63ca2adb36514b68f663b95
aumchayanut <78656093+aumchayanut@users.noreply.github.com>
2023-05-28 14:37:46 +0700

    .sh

---
+++ door.sh
@@ -0,0 +1,1 @@
+ros2 service call /Monitorstart_service custom_nav/srv/Monitorcontinuous "{door1state: 1, door2state: 1}"
---
+++ gamestate.sh
@@ -0,0 +1,1 @@
+ros2 service call /Monitorstart_service custom_nav/srv/Monitorcontinuous "{gamestate: 1, gametime: 120}"
